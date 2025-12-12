ff64961f8ef6c16d9dd364f910c8601f78b5f03d
SashaDyka <beli.rothi2005@gmail.com>
2025-12-12 11:26:11 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bacvkground-red-600">
+        <p>Edit src/App.tsx and save to test HMR</p>
+      </div>
+    </>
+  )
+}
+
+export default App
