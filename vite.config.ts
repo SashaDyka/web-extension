ad83833b94ad6ee07bb51332264fedd327bf83a9
SashaDyka <beli.rothi2005@gmail.com>
2025-12-12 11:28:04 +0200

    feat: edit config vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        popup: 'index.html',
+        background: 'src/background/background.ts',
+        content: 'src/content/content.ts',
+      },
+    },
+  },
 })
